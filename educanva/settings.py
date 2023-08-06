1caa108116137c56c9b246125b6fe83951f93a9e
Aashish <aashishtuladhar09@gmail.com>
2023-08-06 20:09:27 +0545

    Secret removed

--- educanva/settings.py
+++ educanva/settings.py
@@ -197,6 +197,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_HOST_USER = 'aashishtuladhar09@gmail.com'  # The email address from which you want to send the email
-EMAIL_HOST_PASSWORD = 'uzidcqwromkeyrht'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
